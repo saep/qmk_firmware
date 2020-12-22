ed7ede56c008f0b72589b74f5fc4386ced79258f
Sebastian Witte <woozletoff@gmail.com>
2020-12-22 13:58:11 +0100

    Add RGBLIGHT_MODE = yes to rules.mk

--- keyboards/ergodox_ez/keymaps/saep/rules.mk
+++ keyboards/ergodox_ez/keymaps/saep/rules.mk
@@ -1,3 +1,4 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 DYNAMIC_MACRO_ENABLE = yes
+RGBLIGHT_ENABLE = yes
